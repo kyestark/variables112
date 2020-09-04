f3139f20a04af0a120a8be7baafd55412f53bddc
kyestark <70530144+kyestark@users.noreply.github.com>
2020-09-04 13:42:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.body.querySelector("#cool").innerHTML="My name is:"
+
+var firstname = prompt("What is your first name?")
+var lastname = prompt("what is your last name?")
+document.body.querySelector("#lesscool").innerHTML=firstname + " " + lastname
